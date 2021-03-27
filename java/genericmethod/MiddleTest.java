6f2210e54f3be8903b352bc929dda3e012777f74
dzh0622 <dengzaihua@yeah.net>
2021-03-27 21:18:31 +0800

    Create MiddleTest.java

---
+++ java/genericmethod/MiddleTest.java
@@ -0,0 +1,8 @@
+package generic.genericmethod;
+
+public class MiddleTest {
+    public static void main(String[] args) {
+        String middle = ArrayAlg.getMiddle("abc","ert","1243","ooo","5322");
+        System.out.println("The middle element is "+middle);
+    }
+}
